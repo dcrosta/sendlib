c451fd58eb28db5bc3a37706ab769a303bb04d8f
Dan Crosta <dcrosta@gmail.com>
2011-05-28 12:05:28 -0400

    make sendlib easy_installable

--- setup.py
+++ setup.py
@@ -6,11 +6,14 @@ README = file(os.path.join(here, 'README.rst')).read()
 
 setup(
     name='sendlib',
-    version='0.1',
+    version='0.2',
     description='sendlib  is a lightweight message serialization library which aims to be memory efficient',
     long_description=README,
     classifiers=[
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+        'Topic :: Software Development',
         'Topic :: Software Development :: Libraries',
         'License :: OSI Approved :: BSD License',
         'Development Status :: 4 - Beta',
@@ -19,7 +22,7 @@ setup(
     author_email='dcrosta@late.am',
     url='http://github.com/dcrosta/sendlib',
     keywords='message serialization',
-    modules=['sendlib'],
+    py_modules=['sendlib'],
     zip_safe=True,
     test_suite='tests',
 )
