83ecf3b881eead94116332e17c54a3501caf1a78
Dan Crosta <dcrosta@gmail.com>
2011-05-29 15:18:27 -0400

    missing comma

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 2.7',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries',
         'License :: OSI Approved :: BSD License',
