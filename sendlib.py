51ea5412a70cf83a62d51d5c515c0eeac725aea0
Dan Crosta <dcrosta@gmail.com>
2011-05-31 17:16:42 -0400

    version bump => 0.2.1

--- sendlib.py
+++ sendlib.py
@@ -29,7 +29,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = '0.2'
+__version__ = '0.2.1'
 __all__ = ('SendlibError', 'ParseError', 'parse', '__version__')
 
 import codecs
